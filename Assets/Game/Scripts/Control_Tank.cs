f39b2392583546d661db8e77ed6d975858436a85
mugcupeyo <78069896+mugcupeyo@users.noreply.github.com>
2025-10-02 22:51:21 +0900

    탱크 이동

--- Assets/Game/Scripts/Control_Tank.cs
+++ Assets/Game/Scripts/Control_Tank.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class Tank_Move : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody rb;
+    public float speed = 10;
+    public float rotationSpeed = 100f;
+    public float jumpForce = 5f;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        float v = Input.GetAxis("Vertical");   // W/S, ก่/ก้
+        float h = Input.GetAxis("Horizontal"); // A/D, ก็/กๆ
+
+        Vector3 move = transform.forward * v;
+        Quaternion rotation = Quaternion.Euler(0, h * rotationSpeed * Time.fixedDeltaTime, 0);
+
+        rb.MoveRotation(rotation);
+        rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
+
     }
 }
