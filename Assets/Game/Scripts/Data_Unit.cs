1b479feee64e66801c271ee942608c23c19f2abc
kuruku202 <kimmingyu202@naver.com>
2025-10-08 00:55:45 +0900

    1

---
+++ Assets/Game/Scripts/Data_Unit.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Unit
+{
+    int attackDamage;
+    float attackSpeed;
+    int healthMax;
+    int health;
+    int resistance;
+    float moveSpeed;
+
+    public Unit(string type)
+    {
+        attackDamage = 10;
+        attackSpeed = 0.8f;
+        healthMax = 300;
+        health = 300;
+        resistance = 0;
+        moveSpeed = 10;
+    }
+}
+
+public class Fortress : Unit
+{
+    Skill[] skill;
+
+    public Fortress() : base()
+    {
+        skill = new Skill[4];
+    }
+}
