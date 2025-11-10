a021ed393ab1d255c6b84254d638e44e748ca8d0
mugcupeyo <78069896+mugcupeyo@users.noreply.github.com>
2025-11-10 17:32:24 +0900

    정리

--- Assets/Game/Scripts/Enemy_Controler.cs
+++ Assets/Game/Scripts/Enemy_Controler.cs
@@ -1,25 +1,11 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
-    private Nav nav;
+    private NavMove nav;
 
     private void Awake()
     {
-        nav = GetComponent<Nav>();
-    }
-
-    private void Update()
-    {
-        // 마우스 왼쪽 버튼을 눌렀을 때
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
-            {
-                nav.MoveTo(hit.point);
-            }
-        }
+        nav = GetComponent<NavMove>();
     }
 }
